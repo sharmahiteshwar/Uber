2fe2af28edb8aa06ab319445229876d91ae4bef8
Hitesh <aks5145336@gmail.com>
2025-08-12 12:09:32 +0530

    added routes for captain

--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -2,6 +2,7 @@ const captainController = require('../controllers/captain.controller');
 const express = require('express');
 const router = express.Router();
 const {body} = require('express-validator');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 
 
@@ -16,5 +17,16 @@ router.post('/register', [
 ],
     captainController.registerCaptain
 );
+
+router.post('/login', [
+    body('email').isEmail().withMessage('Please enter a valid email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+],
+    captainController.loginCaptain
+);
+
+router.get('/profile', authMiddleware.authCaptain, captainController.getCaptainProfile);
+
+router.get('/logout', authMiddleware.authCaptain, captainController.logoutCaptain);
 
 module.exports = router;
