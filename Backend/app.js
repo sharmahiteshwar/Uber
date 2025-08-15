a35a4c71c6996d1e8bbf7d9123cd7d3bd2e5a9a8
Hitesh <aks5145336@gmail.com>
2025-08-15 11:37:59 +0530

    integrated maps

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
+const mapRoutes = require('./routes/maps.routes');
 
 connectToDb();
 
@@ -23,6 +24,7 @@ app.get('/', (req, res) => {
 
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
+app.use('/maps', mapRoutes);
 
 
 module.exports = app;
